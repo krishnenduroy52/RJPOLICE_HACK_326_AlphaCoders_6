db3c72e866e07aa0830b47a000f6bca809ff7fc3
Indrajit Pal <palindrajit10@gmail.com>
2023-12-22 23:37:47 +0530

    Link changed in navbar

--- Frontend/public/firebase-messaging-sw.js
+++ Frontend/public/firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
 
 const firebaseConfig = {
-    apiKey: "",
+    apiKey: "AIzaSyBY-PXD09ZyOLdsjLVc3Aby6W43BlPdcEo",
     authDomain: "rajasthan-hackathon-project.firebaseapp.com",
     projectId: "rajasthan-hackathon-project",
     storageBucket: "rajasthan-hackathon-project.appspot.com",
