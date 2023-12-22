912c26783009a2594f7121820b4443aee8f568f6
Indrajit Pal <palindrajit10@gmail.com>
2023-12-23 02:53:15 +0530

    Notification functionality added using fifebase

--- Frontend/public/firebase-messaging-sw.js
+++ Frontend/public/firebase-messaging-sw.js
@@ -22,7 +22,10 @@ messaging.onBackgroundMessage((payload) => {
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: payload.notification.image,
+        icon: payload.notification.icon,
+        sound: payload.notification.sound,
+        click_action: payload.notification.click_action,
+
     };
 
     self.registration.showNotification(notificationTitle, notificationOptions);
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,29 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { getToken } from "firebase/messaging";
+import { messaging } from "./firebase";
 // Page element imports
 import Home from "./page/Home";
-
-// Component imports
 import DetailsForm from "./components/userDetails/DetailsForm";
+import { useEffect } from "react";
 
 function App() {
+
+  async function requestNotifyFunction() {
+    const permission = await Notification.requestPermission();
+    if (permission === 'granted') {
+      const deviceToken = await getToken(messaging, { vapidKey: "BHnKjohhTwqxGmmcgVw6VNb5FCZH0rsggroROCIEWBrjS4EAk4WSlakKIKM3xnLh_z3BIDQPD0pi7QP-NtQWooU" });
+      console.log(deviceToken);
+    }
+    else if (permission === 'denied') {
+      alert("You have denied notification permission");
+    }
+  }
+
+  useEffect(() => {
+    requestNotifyFunction();
+  }, [])
   return (
     <>
       <Router>
