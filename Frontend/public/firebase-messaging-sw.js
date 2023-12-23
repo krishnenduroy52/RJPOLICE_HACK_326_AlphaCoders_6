6c8b2fc712a4e5208ab02dc2920881e3fb44412d
krishnendu roy <krishnendu.kalyani1@gmail.com>
2023-12-23 15:11:44 +0530

    firebase foreground notification fixed

--- Frontend/public/firebase-messaging-sw.js
+++ Frontend/public/firebase-messaging-sw.js
@@ -1,38 +1,35 @@
-// import demo from "../src/assets/demo.js"
-
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
-
-// console.log(demo)
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
+);
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBY-PXD09ZyOLdsjLVc3Aby6W43BlPdcEo",
-    authDomain: "rajasthan-hackathon-project.firebaseapp.com",
-    projectId: "rajasthan-hackathon-project",
-    storageBucket: "rajasthan-hackathon-project.appspot.com",
-    messagingSenderId: "1065541948138",
-    appId: "1:1065541948138:web:8a546431c5ec39300d3d93",
-    measurementId: "G-24QQ6YLZB5"
+  apiKey: "AIzaSyBY-PXD09ZyOLdsjLVc3Aby6W43BlPdcEo",
+  authDomain: "rajasthan-hackathon-project.firebaseapp.com",
+  projectId: "rajasthan-hackathon-project",
+  storageBucket: "rajasthan-hackathon-project.appspot.com",
+  messagingSenderId: "1065541948138",
+  appId: "1:1065541948138:web:8a546431c5ec39300d3d93",
+  measurementId: "G-24QQ6YLZB5",
 };
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-    console.log(
-        "[firebase-messaging-sw.js] Received background message ",
-        payload
-    );
+  console.log(messaging);
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
 
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: payload.notification.icon,
-        sound: payload.notification.sound,
-        click_action: payload.notification.click_action,
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon,
+    sound: payload.notification.sound,
+    click_action: payload.notification.click_action,
+  };
 
-    };
-
-    self.registration.showNotification(notificationTitle, notificationOptions);
-
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
