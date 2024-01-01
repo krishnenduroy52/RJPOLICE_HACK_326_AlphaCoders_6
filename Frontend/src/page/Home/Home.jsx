b3345c485dda21e6791694e35055299c5a3a6a64
Anuragkumarsah <asah2307@gmail.com>
2024-01-01 19:22:32 +0530

    added start script

--- Frontend/src/page/Home/Home.jsx
+++ Frontend/src/page/Home/Home.jsx
@@ -4,10 +4,19 @@ const Home = () => {
   return (
     <div className={styles.hero_container}>
       <div>
-        
+        <h1 className={styles.hero_text}>
+          <span className={styles.hero_text_span}>Welcome to</span>
+          <br />
+          <span className={styles.hero_text_span}>the</span>
+          <br />
+          <span className={styles.hero_text_span}>Covid-19</span>
+          <br />
+          <span className={styles.hero_text_span}>Tracker</span>
+        </h1>
       </div>
       <div>
         <video
+          className={styles.hero_video}
           data-v-27edef16=""
           data-test="video-content"
           preload="auto"
