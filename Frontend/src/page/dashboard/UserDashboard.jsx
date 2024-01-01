82e71479c633b0e276edb4ce4ebba84be1df7bc1
krishnendu roy <krishnendu.kalyani1@gmail.com>
2024-01-01 20:56:12 +0530

    card added

--- Frontend/src/page/dashboard/UserDashboard.jsx
+++ Frontend/src/page/dashboard/UserDashboard.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Card1 from "../../components/card/card1/Card1";
 
 const UserDashboard = () => {
-  return <div>userDashboard</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: "white",
+        padding: "4rem 1rem",
+        borderRadius: "30px 30px 30px 30px",
+      }}
+    >
+      <h1>User Dashboard</h1>
+      <Card1 />
+    </div>
+  );
 };
 
 export default UserDashboard;
