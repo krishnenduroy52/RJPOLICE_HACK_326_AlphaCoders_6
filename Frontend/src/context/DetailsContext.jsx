c235f0ff568a3c9936e87000617b248e3f8f54bf
Indrajit Pal <palindrajit10@gmail.com>
2024-01-01 21:31:00 +0530

    user's location push backend

--- Frontend/src/context/DetailsContext.jsx
+++ Frontend/src/context/DetailsContext.jsx
@@ -5,6 +5,12 @@ export const DetailsContext = createContext();
 const DetailsProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isAdmin, setIsAdmin] = useState(false);
+  const [locationValues, setLocationValues] = useState(null);
+
+  const handleLngLatRotationValue = (values) => {
+    console.log(values);
+    setLocationValues(values);
+  }
 
   useEffect(() => {
     const userDetails = localStorage.getItem("user");
@@ -14,7 +20,7 @@ const DetailsProvider = ({ children }) => {
   }, []);
   return (
     <DetailsContext.Provider
-      value={{ test: "Test", user: user, isAdmin: isAdmin }}
+      value={{ test: "Test", user: user, isAdmin: isAdmin, handleLngLatRotationValue, locationValues}}
     >
       {children}
     </DetailsContext.Provider>
