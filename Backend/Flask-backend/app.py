b589891a7c580bcdc7aa04f3483b34630b40c4cd
krishnendu roy <krishnendu.kalyani1@gmail.com>
2024-01-05 22:17:37 +0530

    license plate done

--- Backend/Flask-backend/app.py
+++ Backend/Flask-backend/app.py
@@ -98,6 +98,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 from gun_detection import gun_find
+from licence_plate import license_plate_find
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -110,9 +111,14 @@ def hello():
 
 @app.route('/gun-detection', methods=['POST'])
 def handle_gun_detection():
-    print("Handling gun detection")
-    return gun_find(request)
+    return license_plate_find(request)
+    # return gun_find(request)
 
+
+@app.route('/license-plate-detection', methods=['POST'])
+def handle_license_plate_detection():
+    return license_plate_find(request)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
