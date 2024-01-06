87f32a654255b201475ba29be347af0f9d714e41
Indrajit Pal <palindrajit10@gmail.com>
2024-01-06 16:40:33 +0530

    number-plate-page-changed

--- Frontend/src/page/NumberPlateDetection/NumPlateDetection.jsx
+++ Frontend/src/page/NumberPlateDetection/NumPlateDetection.jsx
@@ -5,7 +5,7 @@ const NumPlateDetection = () => {
 
     const inputRef = useRef(null);
 
-    const sendPlateNumber = async() => {
+    const sendPlateNumber = async () => {
         const number_plate = inputRef.current.value;
         const url = `http://localhost:8000/sendnumber?value=${encodeURIComponent(number_plate)}`;
         const data = await fetch(url, {
@@ -32,7 +32,7 @@ const NumPlateDetection = () => {
                 alignItems: 'center',
                 height: '100%',
                 gap: '5rem',
-            
+
             }}>
                 <h1 style={{
                     fontSize: '3rem',
@@ -41,14 +41,25 @@ const NumPlateDetection = () => {
                     textAlign: 'center',
                     margin: '0px',
                     padding: '0px',
-                
+
                 }}>Number Plate Detection</h1>
+                <div className='flex justify-between items-center'>
+                    <button className='m-2 bg-pink-500 hover:bg-pink-400 text-white font-bold py-2 px-4 border-b-4 border-pink-700 hover:border-pink-500 rounded-xl'>
+                        Start Webcam
+                    </button>
+                    <span className='font-bold'>or</span>
+                    <button className='m-2 bg-pink-500 hover:bg-pink-400 text-white font-bold py-2 px-4 border-b-4 border-pink-700 hover:border-pink-500 rounded-xl'>
+                        Using video
+                    </button>
+                </div>
                 <input style={{
+                    backgroundColor: 'pink',
                     width: '200px',
                     height: '40px',
                     fontSize: '20px',
-                    padding: '10px'
-                }} ref={inputRef} type='string'/>
+                    padding: '10px',
+                    borderRadius: '10px',
+                }} ref={inputRef} type='string' />
                 <button style={{
                     width: '100px',
                     height: 'auto',
