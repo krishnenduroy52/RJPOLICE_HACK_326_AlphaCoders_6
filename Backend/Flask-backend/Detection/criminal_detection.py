ac56e781e85589aaa4e2a88a9e793019747dec7c
krishnendu roy <krishnendu.kalyani1@gmail.com>
2024-01-08 12:05:08 +0530

    fire detection fixed and upload image moved to util file

--- Backend/Flask-backend/Detection/criminal_detection.py
+++ Backend/Flask-backend/Detection/criminal_detection.py
@@ -1,10 +1,13 @@
 from deepface import DeepFace
 import cv2
+import numpy as np
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
 
 def criminal_detection(request):
-    # get two image imageref and image from request 
+    # get two image imageref and image from request
     image = request.files['image'].read()
     image = cv2.imdecode(np.frombuffer(image, np.uint8), -1)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -12,4 +15,3 @@ def criminal_detection(request):
     imageref = cv2.imdecode(np.frombuffer(imageref, np.uint8), -1)
     imageref = cv2.cvtColor(imageref, cv2.COLOR_BGR2RGB)
     # detect face in image
-    
