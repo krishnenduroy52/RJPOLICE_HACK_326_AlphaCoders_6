92a8ecd3235c36c79cb8e24c72ca3955cb8478a9
Indrajit Pal <palindrajit10@gmail.com>
2024-01-11 01:28:26 +0530

    evidence section modal of viewing map added with realtime user data

--- Frontend/src/page/Evidence/CrimeEvidence.jsx
+++ Frontend/src/page/Evidence/CrimeEvidence.jsx
@@ -5,85 +5,87 @@ import styles from "./CrimeEvidence.module.css";
 import CardEvidence from "../../components/card/CardEvidence/CardEvidence";
 
 const CrimeEvidence = () => {
-  const { evidence } = useContext(DetailsContext);
-  const [cameraEvidence, setCameraEvidence] = useState([]);
+    const { evidence } = useContext(DetailsContext);
+    const [cameraEvidence, setCameraEvidence] = useState([]);
 
-  const categories = [
-    {
-      img_url: "https://i.imgur.com/7NXZD71.png",
-      name: "All",
-    },
-    {
-      img_url: "https://i.imgur.com/7NXZD71.png",
-      name: "Gun",
-    },
-    {
-      img_url: "https://i.imgur.com/ViQfcxb.png",
-      name: "Fire",
-    },
-    {
-      img_url: "https://i.imgur.com/Z6vk1wg.png",
-      name: "Number Plate",
-    },
-    {
-      img_url: "https://i.imgur.com/S5cg6iS.png",
-      name: "Criminal",
-    },
-  ];
+    const categories = [
+        {
+            img_url: "https://i.imgur.com/7NXZD71.png",
+            name: "All",
+        },
+        {
+            img_url: "https://i.imgur.com/7NXZD71.png",
+            name: "Gun",
+        },
+        {
+            img_url: "https://i.imgur.com/ViQfcxb.png",
+            name: "Fire",
+        },
+        {
+            img_url: "https://i.imgur.com/Z6vk1wg.png",
+            name: "Number Plate",
+        },
+        {
+            img_url: "https://i.imgur.com/S5cg6iS.png",
+            name: "Criminal",
+        },
+    ];
 
-  useEffect(() => {
-    setCameraEvidence(evidence);
-  }, [evidence]);
+    useEffect(() => {
+        setCameraEvidence(evidence);
+    }, [evidence]);
 
-  const handleSelection = (e) => {
-    const query =
-      e.target.innerText == "Gun"
-        ? "Gun detected"
-        : e.target.innerText == "Fire"
-        ? "Fire detected"
-        : e.target.innerText == "Number Plate"
-        ? "number_plate"
-        : e.target.innerText == "Criminal"
-        ? "Criminal detected"
-        : "all";
-    if (query === "all") return setCameraEvidence(evidence);
-    else {
-      const filterEvidences = evidence.filter((item) => item.crime === query);
-      setCameraEvidence(filterEvidences);
-    }
-  };
-  return (
-    <div
-      style={{
-        backgroundColor: "#F8F6F5",
-        padding: "4rem 1rem",
-        borderRadius: "30px 30px 30px 30px",
-      }}
-    >
-      <div className={styles.one}>
-        <h1 className={styles.headline}>Activity Evicence</h1>
-      </div>
-      <div className={styles.evidenceContainer}>
-        <div className={styles.categories}>
-          {categories.map((category) => (
-            <div className={styles.category} onClick={handleSelection}>
-              <img
-                src={category.img_url}
-                alt={category.name}
-                className={styles.icon}
-              />
-              <div>{category.name}</div>
+    const handleSelection = (e) => {
+        const query =
+            e.target.innerText == "Gun"
+                ? "Gun detected"
+                : e.target.innerText == "Fire"
+                    ? "Fire detected"
+                    : e.target.innerText == "Number Plate"
+                        ? "number_plate"
+                        : e.target.innerText == "Criminal"
+                            ? "Criminal detected"
+                            : "all";
+        if (query === "all") return setCameraEvidence(evidence);
+        else {
+            const filterEvidences = evidence.filter((item) => item.crime === query);
+            setCameraEvidence(filterEvidences);
+        }
+    };
+    return (
+        <div
+            style={{
+                backgroundColor: "#F8F6F5",
+                padding: "4rem 1rem",
+                borderRadius: "30px 30px 30px 30px",
+            }}
+        >
+            <div className={styles.one}>
+                <h1 className={styles.headline}>Activity Evidence</h1>
             </div>
-          ))}
+            <div className={styles.evidenceContainer}>
+                <div className={styles.categories}>
+                    {categories.map((category) => (
+                        <div className={styles.category} onClick={handleSelection}>
+                            <img
+                                src={category.img_url}
+                                alt={category.name}
+                                className={styles.icon}
+                            />
+                            <div>{category.name}</div>
+                        </div>
+                    ))}
+                </div>
+                <div className={styles.evidence}>
+                    <div className="">
+                        {cameraEvidence.map((evi, idx) => (
+                            <CardEvidence key={evi.id} evi={evi} />
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="evidences">
-          {cameraEvidence.map((evi, idx) => (
-            <CardEvidence key={evi.id} evi={evi} />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default CrimeEvidence;
