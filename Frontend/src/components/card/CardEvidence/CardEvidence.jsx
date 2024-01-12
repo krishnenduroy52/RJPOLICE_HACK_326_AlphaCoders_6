dc413a0463b145585020fa24810dc3db20ce21e3
Indrajit Pal <palindrajit10@gmail.com>
2024-01-12 15:12:16 +0530

    class changed to classname

--- Frontend/src/components/card/CardEvidence/CardEvidence.jsx
+++ Frontend/src/components/card/CardEvidence/CardEvidence.jsx
@@ -12,7 +12,7 @@ function CenteredModal({ show, onHide, coordinates, userId }) {
           onClick={onHide}
           className={style.closeButton}
         >
-          <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+          <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
@@ -53,7 +53,7 @@ const CardEvidence = ({ evi }) => {
         show={modalShow}
         onHide={() => setModalShow(false)}
         coordinates={evi.location}
-        userId = {evi.userid}
+        userId={evi.userid}
       />
     </div>
   );
