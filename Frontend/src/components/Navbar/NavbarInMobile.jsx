2e900e9777d70552b3a0d41f7eb9ccb1e2335a10
Anuragkumarsah <asah2307@gmail.com>
2024-01-16 12:22:06 +0530

    fixed nabar

--- Frontend/src/components/Navbar/NavbarInMobile.jsx
+++ Frontend/src/components/Navbar/NavbarInMobile.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './NavbarInMobile.module.css';
 
 const NavbarInMobile = () => {
     const [isNavbarOpen, setNavbarOpen] = useState(false);
+    const navActive = useRef(null);
 
     const toggleNavbar = () => {
-        setNavbarOpen(!isNavbarOpen);
+            setNavbarOpen(!isNavbarOpen);
     };
 
     return (
@@ -27,7 +28,7 @@ const NavbarInMobile = () => {
                         >
                             <span className="sr-only">Close main menu</span>
                             <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                             </svg>
                         </button>
                     ) : (
@@ -40,7 +41,7 @@ const NavbarInMobile = () => {
                         >
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
                             </svg>
                         </button>
                     )}
@@ -49,7 +50,7 @@ const NavbarInMobile = () => {
 
             </div>
             <div>
-                <div className={styles.mobileMenu}>
+                <div ref={navActive} className={`${styles.mobileMenu} ${isNavbarOpen ? styles.isOpen : ''}` }>
                     <div>
                         <li><Link to={'/'} className="menu__item">Home</Link></li>
 
